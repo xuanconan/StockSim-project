919ba00c839bc05ec08d8d6550866f1a9f575f2f
conanxuan <xuan.k@husky.neu.edu>
2017-11-13 12:01:13 -0500

    heroku

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 // initialize parser for json
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Point static path to dist -- For building -- REMOVE
 app.use(express.static(path.join(__dirname, 'dist')));
