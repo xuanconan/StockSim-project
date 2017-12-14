e7f7c3b78ab57b3ac56af90088b1076231cec899
conanxuan <xuan.k@husky.neu.edu>
2017-12-13 19:45:12 -0500

    heroku

--- assignment/models/model.js
+++ assignment/models/model.js
@@ -1,7 +1,7 @@
 //require mongoose and connect to database
 var mongoose = require('mongoose');
 
-var connectionString = 'mongodb://localhost/cs5610';
+// var connectionString = 'mongodb://localhost/cs5610';
 
 if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
   var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
@@ -15,7 +15,7 @@ if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
 // hw6: var connectionString = 'mongodb://admin:admin@ds133964.mlab.com:33964/heroku_1gbtpnhz';
 //                                                    ds133964.mlab.com:33964/heroku_1gbtpnhz
 
-// var connectionString = 'mongodb://admin:admin@ds125565.mlab.com:25565/heroku_p8cl5qsb';
+var connectionString = 'mongodb://admin:admin@ds125565.mlab.com:25565/heroku_p8cl5qsb';
 
 
 //                                            ds133856.mlab.com:33856/heroku_gdrfzfcv
