4c1000c57b6c0dec595047fa4b2ce09501fa3c2f
conanxuan <xuan.k@husky.neu.edu>
2017-12-13 21:09:57 -0500

    final4

--- assignment/models/model.js
+++ assignment/models/model.js
@@ -10,6 +10,8 @@ if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
   connectionString += '@ds125565.mlab.com:25565/heroku_p8cl5qsb'; // use yours
 }
 
+
+// purple: mongodb://heroku_bwv17737:utdvd8kcbatr6ubhdfokfm1m5p@ds019068.mlab.com:19068/heroku_bwv17737
 // mongodb://heroku_gdrfzfcv:hjnmv3ti0mcdq24s8u58sdckk7@ds133856.mlab.com:33856/heroku_gdrfzfcv
 
 // hw6: var connectionString = 'mongodb://admin:admin@ds133964.mlab.com:33964/heroku_1gbtpnhz';
